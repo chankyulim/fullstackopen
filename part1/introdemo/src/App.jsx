4164d080f68c2e9589a3eec4669121224fddb2cb
Chan Lim <chan.lim@caspio.com>
2025-04-11 10:21:46 -0400

    Part 1.1

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -16,12 +16,16 @@ const Footer = () => {
 }
 
 const App = () => {
-  const friends = ['Peter', 'Maya']
+  const name = 'Peter'
+  const age = 10
 
   return (
-    <>
-      <p>{friends}</p>
-    </>
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </div>
   )
 }
 
