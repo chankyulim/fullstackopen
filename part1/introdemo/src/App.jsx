e93a8e859176ae8c409d9b4a3e01e57f348e51dc
Chan Lim <chan.lim@caspio.com>
2025-04-11 09:52:15 -0400

    introdemo done

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
 
-function App() {
-  const [count, setCount] = useState(0)
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/chankyulim'>chankyulim</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = ['Peter', 'Maya']
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>{friends}</p>>
     </>
   )
 }
